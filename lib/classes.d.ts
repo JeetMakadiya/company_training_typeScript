2a745e9ef64ff65622302ab48305c68cff87bcfa
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-01-31 10:19:54 +0530

    TypeScript Classes Practice

---
+++ lib/classes.d.ts
@@ -0,0 +1,8 @@
+declare class Students {
+    private fname;
+    private lname;
+    constructor(fname: string, lname: string);
+    getFullName(): string;
+}
+declare let s1: Students;
+declare let s2: Students;
